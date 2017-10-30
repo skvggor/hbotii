5c9b389c5e38ece4e04efd40b77fa28bdec30382
Marcos Garcia de Lima <marckfree@gmail.com>
2017-10-30 15:59:35 -0200

    Add coffee interactions

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function onOnline() {
 }
 
 function onMessage(from, message) {
-	//  marckfree@gmail.com
-	// const marcker = '0b1xmm5976r0k266dub553id64@public.talk.google.com/lcsw_hangouts_135E5474'
-
 	const greetingsDict = [
 		'e ai',
 		'oi',
@@ -83,10 +80,32 @@ function onMessage(from, message) {
 		'sou mesmo um bot (hbotii: https://github.com/marcker/hbotii)'
 	]
 
+	const coffeeDict = [
+		'coffee',
+		'café',
+		'cafe',
+		'chá',
+		'cha',
+		'tomar café',
+		'tomar cafe',
+		'tomar chá',
+		'tomar cha'
+	]
+
+	const coffeeAnswers = [
+		'Vou sim, só um segundo.',
+		'bora, as 17h?',
+		'to afim, \'usou a cabeça\' hoje? Chegou cedão...',
+		'Por mim vamos sim!',
+		'Vamos... Chama os caras ai.',
+		'Já já vamos. Só um minuto; matar uma coisa aqui.'
+	]
+
 	let helpOccurrences = 0
 	let greetingsOccurrences = 0
 	let offensesAndOthersOccurrences = 0
 	let botOccurrences = 0
+	let coffeeOccurrences = 0
 
 	message = message.toLowerCase()
 
@@ -110,11 +129,17 @@ function onMessage(from, message) {
 		if (message.includes(botDict[i])) botOccurrences += 1
 	}
 
+	// coffee
+	for (let i = 0, len = coffeeDict.length; i < len; i += 1) {
+		if (message.includes(coffeeDict[i])) coffeeOccurrences += 1
+	}
+
 	// horrible I knows... TODO: Refact, refact and refact
 	if (greetingsOccurrences > 0) bot.sendMessage(from, greetingsAnswers[Math.floor(Math.random() * greetingsAnswers.length)])
 	if (offensesAndOthersOccurrences > 0) bot.sendMessage(from, offensesAndOthersAnswers[Math.floor(Math.random() * offensesAndOthersAnswers.length)])
 	if (helpOccurrences > 0) bot.sendMessage(from, helpAnswers[Math.floor(Math.random() * helpAnswers.length)])
 	if (botOccurrences > 0) bot.sendMessage(from, botAnswers[Math.floor(Math.random() * botAnswers.length)])
+	if (coffeeOccurrences > 0) bot.sendMessage(from, coffeeAnswers[Math.floor(Math.random() * coffeeAnswers.length)])
 }
 
 bot.on('online', onOnline)
