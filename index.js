9d4eacb12f4d6556c8fc432ea98a7e97f65bbabd
Marcos Garcia de Lima <marckfree@gmail.com>
2017-12-15 16:42:28 -0200

    Add dialogflow (ai improvement)

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const path = require('path')
 const port = process.env.PORT || 8080
 
 const hangoutsBot = require('hangouts-bot')
+const apiAi = require('apiai')
+const uuidv4 = require('uuid/v4')
 
 const user = process.env.HANGOUTS_USER
 const password = process.env.HANGOUTS_PASSWORD
+const dialogFlow = apiAi(process.env.DIALOGFLOW_ACCESSTOKEN)
 
 const bot = new hangoutsBot(user, password)
 
@@ -21,116 +24,12 @@ function onOnline() {
 }
 
 function onMessage(from, message) {
-	const greetingsDict = [
-		'e ai',
-		'oi',
-		'como vai',
-		'tudo bem?',
-		'fala ai',
-		'eai',
-		'mano'
-	]
+	const options = { sessionId: uuidv4() }
+	const request = dialogFlow.textRequest(message, options)
 
-	const greetingsAnswers = [
-		'bot: Opa, como vai? Tudo certo? Por aqui beleza.',
-		'bot: Tudo bem?',
-		'bot: Por aqui tranquilo.',
-		'bot: Só de boa?',
-		'bot: Opa, tudo beleza!'
-	]
-
-	const offensesAndOthersDict = [
-		'nada a ver',
-		'gay',
-		'viado',
-		'cuzao',
-		'mestre'
-	]
-
-	const offensesAndOthersAnswers = [
-		'bot: Que isso novinho... Não abusa.',
-		'bot: Se liga pô...',
-		'bot: É nada.',
-		'bot: Olha as palavras...',
-		'bot: Nem brinca com isso, se não vou ter uma recaída.'
-	]
-
-	const helpDict = [
-		'help',
-		'ajuda',
-		'ajudar',
-		'força',
-		'ocupado',
-		'fudido'
-	]
-
-	const helpAnswers = [
-		'bot: Opa, só um segundo :-)',
-		'bot: Como assim, diz ai.',
-		'bot: Fala, mestre! Pode falar o que precisa.',
-		'bot: Posso sim, o que foi?',
-		'bot: Me manda, ai a gente vê :-)'
-	]
-
-	const botDict = [
-		'bot',
-		'robo',
-		'robô',
-		'android'
-	]
-
-	const botAnswers = [
-		`bot: Você sacou - sou um bot mesmo. Meu nome é hbotii e estou vivo em
-		https://github.com/marcker/hbotii. Meu criador fez um código pobre, mas
-		ele prometeu me refatorar ;-)`,
-		`bot: Você descobriu que sou um bot: hbotii é como me chamam e estou em
-		https://github.com/marcker/hbotii. Meu criador fez um código pobre, mas
-		ele prometeu me refatorar ;-)`,
-		'bot: sou mesmo um bot (hbotii: https://github.com/marcker/hbotii)'
-	]
-
-	const coffeeDict = [
-		'coffee',
-		'café',
-		'cafe',
-		'chá',
-		'cha',
-		'tomar café',
-		'tomar cafe',
-		'tomar chá',
-		'tomar cha'
-	]
-
-	const coffeeAnswers = [
-		'bot: Vou sim, só um segundo.',
-		'bot: bora, às 17h?',
-		'bot: to afim, \'usou a cabeça\' hoje? Chegou cedão...',
-		'bot: Por mim vamos sim!',
-		'bot: Vamos... Chama os caras ai.',
-		'bot: Já já vamos. Só um minuto; matar uma coisa aqui.'
-	]
-
-	message = message.toLowerCase()
-
-	function shakeArray(answers) {
-		return answers[Math.floor(Math.random() * answers.length)]
-	}
-
-	function talk(dictArray, answers) {
-		let accumulator = 0
-
-		for (let i = 0, len = dictArray.length; i < len; i += 1) {
-			if (message.includes(dictArray[i])) accumulator += 1
-		}
-
-		if (accumulator > 0) bot.sendMessage(from, shakeArray(answers))
-	}
-
-	talk(greetingsDict, greetingsAnswers)
-	talk(offensesAndOthersDict, offensesAndOthersAnswers)
-	talk(botDict, botAnswers)
-	talk(helpDict, helpAnswers)
-	talk(coffeeDict, coffeeAnswers)
+	request.on('response', response => bot.sendMessage(from, response.result.fulfillment.speech))
+	request.on('error', error => console.error(error))
+	request.end()
 }
 
 bot.on('online', onOnline)
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 # [hbotii](http://hbotii.herokuapp.com/)
+> Google Hangouts bot playground.
 
-Google Hangouts bot playground.
+---
+
+Built with :heart and:
+
+- [DialogFlow](https://dialogflow.com)
+- [Nodejs](https://nodejs.org)
+- [Api.ai](https://www.npmjs.com/package/apiai)
+- [nodemon](https://nodemon.io/)
+- [Express](https://expressjs.com/)
+- [hangouts-bot](https://github.com/jaxbot/hangouts-bot)
+- Host: [Heroku](https://www.heroku.com/)
